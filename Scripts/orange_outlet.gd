b9ccdca32b60d5a2ffccd2b6fa4a62cecf0c5f34
JimmyNeutralz <hecimovichd9339@my.uwstout.edu>
2025-09-17 12:17:16 -0500

    Increased Z index of Orange outlet

---
+++ Scripts/orange_outlet.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var isConnected = true
+
+
+func _process(delta):
+	
+	# Test for custom input actions
+	if (Input.is_action_just_pressed("Connect Orange Wire") and !isConnected):
+		$AnimatedSprite2D.modulate = Color(0, 0, 1)
+		print("Orange Wire Connected")
+		isConnected = true
+	elif (Input.is_action_just_pressed("Disconnect Orange Wire") and isConnected):
+		$AnimatedSprite2D.modulate = Color(1, 1, 1)
+		print("Orange Wire Disconnected")
+		isConnected = false
--- Scripts/test_player.gd
+++ Scripts/test_player.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@onready var hitBox = $CollisionShape2D
 
+func start():
+	hitBox.disabled = false
+	
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
@@ -14,3 +18,5 @@ func _physics_process(delta):
 func _die():
 	queue_free()
 	print ("YOU DIED!")
+
+	
