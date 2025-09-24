c0b4af51e1e58feadc9b101689109fcded8dc415
JimmyNeutralz <hecimovichd9339@my.uwstout.edu>
2025-09-24 13:02:06 -0500

    Finishing Prototype

--- Scripts/area_2d.gd
+++ Scripts/area_2d.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+#@export var simultaneous_scene = preload("res://Scenes/Floor1_Lobby.tscn").instantiate()
+
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		get_tree().change_scene_to_file("res://Floor1_Lobby.tscn")
+		#get_node("/root/Floor1FirstRoom").hide()
+		get_tree().change_scene_to_file("res://Scenes/Floor1_Lobby_After_Vaccuum.tscn")
+		#get_tree().change_scene_to_file("res://Floor1_Lobby.tscn")
--- Scripts/blue_outlet.gd
+++ Scripts/blue_outlet.gd
@@ -7,10 +7,10 @@ func _process(delta):
 	
 	# Test for custom input actions
 	if (Input.is_action_just_pressed("Connect Purple Wire") and !isConnected):
-		#$AnimatedSprite2D.modulate = Color(0, 0, 0)
+		
 		print("Blue Wire Connected")
 		isConnected = true
 	elif (Input.is_action_just_pressed("Disconnect Purple Wire") and isConnected):
-		#$AnimatedSprite2D.modulate = Color(1, 1, 1)
+		
 		print("Blue Wire Disconnected")
 		isConnected = false
