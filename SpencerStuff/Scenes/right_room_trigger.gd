40cab034e96601a88b0bf7210b5f60dfffc95f61
Spencer <spencerbetchey@gmail.com>
2025-09-30 02:53:57 -0500

    Camera switch works

--- SpencerStuff/Scenes/right_room_trigger.gd
+++ SpencerStuff/Scenes/right_room_trigger.gd
@@ -1,5 +1,12 @@
 extends Area3D
 
+@onready var camera1 = $"../../Cameras/Camera1"
+@onready var camera2 = $"../../Cameras/Camera2"
+@onready var camera3 = $"../../Cameras/Camera3"
 
 func _on_body_entered(body: Node3D) -> void:
-	print("Player entered right room")
+	if body.name == "Player":
+		print("Player entered right room")
+		camera1.current = false
+		camera2.current = false
+		camera3.current = true
--- SpencerStuff/Scripts/left_room_trigger.gd
+++ SpencerStuff/Scripts/left_room_trigger.gd
@@ -1,5 +1,12 @@
 extends Area3D
 
+@onready var camera1 = $"../../Cameras/Camera1"
+@onready var camera2 = $"../../Cameras/Camera2"
+@onready var camera3 = $"../../Cameras/Camera3"
 
 func _on_body_entered(body: Node3D) -> void:
-	print("Player entered left room")
+	if body.name == "Player":
+		print("Player entered left room")
+		camera1.current = false
+		camera2.current = true
+		camera3.current = false
