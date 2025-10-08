5c8848032ea67521e59e5b1172b4a5a2da95e361
Komasangamer-coding <stolzd2061@my.uwstout.edu>
2025-10-08 13:15:51 -0500

    Varied Auto Movement Code Experimentation

---
+++ Scripts/puddle_colider.gd
@@ -0,0 +1,20 @@
+extends Area3D
+
+@onready var player = load("res://Scenes/3d_player.tscn")
+@onready var puddle = load("res://Scripts/death_puddle_3d.gd")
+
+var electrified = true
+
+
+func _process(delta):
+	if (Input.is_action_just_pressed("Disable 3D Puddle Electricity")):
+		electrified = false
+	elif (Input.is_action_just_pressed("Enable 3D Puddle Electricity")):
+		electrified = true
+		
+
+func _on_body_entered(body: Node3D) -> void:
+	print("Func reached")
+	if (electrified):
+		print("If reached")
+		get_tree().call_group("Player", "stop_moving")
