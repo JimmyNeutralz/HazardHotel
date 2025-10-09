463d03ce637e05f9673d24862041e49bf7f1a77c
Komasangamer-coding <stolzd2061@my.uwstout.edu>
2025-10-09 14:30:34 -0500

    Figured Out The Issue With the Floor Not Saving

---
+++ Scripts/puddle_colider.gd
@@ -0,0 +1,21 @@
+extends Area3D
+
+#@onready var player = load("res://Scenes/3d_player.tscn")
+@onready var player = $"../3dPlayer"
+@onready var puddle = load("res://PrototypeScripts/death_puddle_3d.gd")
+
+var electrified = true
+
+
+func _process(delta):
+	if (Input.is_action_just_pressed("Disable 3D Puddle Electricity")):
+		electrified = false
+	elif (Input.is_action_just_pressed("Enable 3D Puddle Electricity")):
+		electrified = true
+		
+
+func _on_body_entered(body: Node3D) -> void:
+	print("Func reached")
+	if (electrified):
+		print("If reached")
+		player.stop_moving()
