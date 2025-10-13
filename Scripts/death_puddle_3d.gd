04c0608835775db85580caf36adc5349248a6a30
Komasangamer-coding <stolzd2061@my.uwstout.edu>
2025-10-12 20:14:47 -0500

    Added some comments for guidance

--- Scripts/death_puddle_3d.gd
+++ Scripts/death_puddle_3d.gd
@@ -2,11 +2,15 @@ extends Area3D
 @onready var player = $"../3dPlayer"
 @onready var collideshape = get_node("CollisionShape3D")
 
+#Var to determine if the puddle is deadly or not
 var unsafe = true
 
+#Changes the unsafe var whenever the puddle is deactivated or reactivated
 func change_puddle_status(electrified):
 	unsafe = electrified
 
+#Function that if the player is standing in the puddle 
+# while it's electrified, will cause them to die
 func determine_status(inDanger):
 	if (inDanger && unsafe):
 		print("If met")
