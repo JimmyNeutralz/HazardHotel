46189c97197ee6d19f2bae3feee73f14ffef417e
JimmyNeutralz <hecimovichd9339@my.uwstout.edu>
2025-10-24 13:46:31 -0500

    Implement TV Static on Beta Build

--- Donovan/fade_in_view.gd
+++ Donovan/fade_in_view.gd
@@ -15,10 +15,11 @@ func _fade_static_in():
 	await tween.finished
 	tween.kill()
 
+
 func _exit_scene(next_scene_path):
 	var tween = get_tree().create_tween()
 	tween.tween_property(trueTVFuzz, "modulate", Color(1, 1, 1, 1), 1.5)
 	await tween.finished
+	tween.kill()
 	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file(next_scene_path)
-	tween.kill()
--- SpencerStuff/Scripts/level_management.gd
+++ SpencerStuff/Scripts/level_management.gd
@@ -5,15 +5,24 @@ extends Node3D
 @onready var right_key = $RightWall/RightKey
 @onready var elevator_lock = $Elevator/ElevatorLock
 @onready var generator = $Generator
-@onready var fade_in_static = $Cameras/Camera/FadeInView
+@onready var fade_in_static:Node3D = $Cameras/Camera/FadeInView
+
+@onready var pause_menu_background = $PauseMenu/PauseBackground
+@onready var resume_button = $PauseMenu/ResumeButton
+@onready var quit_button = $PauseMenu/QuitButton
 
 @export var next_scene_path := "res://Donovan/MODIFIEDAlphaV4.tscn"
+@export var main_menu_scene = "res://SpencerStuff/Scenes/MainMenu.tscn"
 
 #Key states
 var has_left_key: bool = false
 var has_right_key: bool = false
 
 func _ready():
+	#Start hidden
+	_set_pause_menu_visible(false)
+	if(!fade_in_static):
+		fade_in_static = $Cameras/Camera/FadeInView
 	fade_in_static._fade_static_out()
 
 
@@ -48,6 +57,58 @@ func _check_keys() -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if generator.activated:
 		#Global.fade_to_level_2()
-		fade_in_static._exit_scene(next_scene_path)
+		fade_in_static._exit_scene("res://BetaBuild/Scenes/BetaV1SecondFloor.tscn")
 		#fade_in_static._fade_static_in()
 		#get_tree().change_scene_to_file(next_scene_path)
+		
+func _input(event):
+
+	if event.is_action_pressed("pause"):
+
+		if get_tree().paused:
+
+			_resume_game()
+
+		else:
+
+			_pause_game()
+ 
+#Pause/Resume Helper
+
+func _pause_game():
+
+	_set_pause_menu_visible(true)
+
+	get_tree().paused = true  #Pause everything in the scene tree (freeze scene)
+
+	print("Game Paused")
+ 
+func _resume_game():
+
+	_set_pause_menu_visible(false)
+
+	get_tree().paused = false  #Resume the scene tree (unfreeze)
+
+	print("Game Resumed")
+ 
+func _set_pause_menu_visible(visible: bool) -> void:
+
+	pause_menu_background.visible = visible
+
+	resume_button.visible = visible
+
+	quit_button.visible = visible
+ 
+#Button calls
+
+func _on_resume_pressed():
+
+	_resume_game()
+ 
+func _on_quit_pressed():
+
+	get_tree().paused = false  #Unpause before leaving so user can interact with main menu
+
+	get_tree().change_scene_to_file(main_menu_scene)
+ 
+		
