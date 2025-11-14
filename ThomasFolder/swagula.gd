a416c5e45df43b3ee4d99fb7e1bb23a4ebcaa8d0
Komasangamer-coding <stolzd2061@my.uwstout.edu>
2025-11-13 23:07:24 -0600

    Removed input for Swagula and added in the dialogue box to FirstPuzzle

--- ThomasFolder/swagula.gd
+++ ThomasFolder/swagula.gd
@@ -8,8 +8,6 @@ var talked_with = false
 
 func _process(delta: float) -> void:
 	if (!talked_with):
-		if (Input.is_action_just_pressed("Talk to Swagula")):
-			player.move_to_specific_location(swagula_marker.global_position.x)
 		if ((player.global_position.x > (swagula_marker.global_position.x - 0.5)) and (player.global_position.x < (swagula_marker.global_position.x + 0.5))):
 			text_popup.change_text_image(2)
 			text_popup.set_text("Hey guys its me swagula", 4)
