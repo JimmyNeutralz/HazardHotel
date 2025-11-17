b42b4ec4e441714894b9d8de88d58b6857e300f8
Spencer <spencerbetchey@gmail.com>
2025-11-16 23:46:48 -0600

    Fixed end scene transition issue

--- SpencerStuff/Scripts/level_management.gd
+++ SpencerStuff/Scripts/level_management.gd
@@ -113,10 +113,13 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		await get_tree().create_timer(4.0).timeout #4 seconds, adjust as desired
 
 		#Now fade and change scene
-		if get_tree().current_scene.name == "FirstPuzzle":
+		var path := get_tree().current_scene.scene_file_path
+
+		if path == "res://Donovan/Puzzle Concepts In Engine/Scenes/FirstPuzzle.tscn":
 			fade_in_static._exit_scene("res://SpencerStuff/Scenes/EndScene.tscn")
 		else:
 			fade_in_static._exit_scene("res://Donovan/Puzzle Concepts In Engine/Scenes/FirstPuzzle.tscn")
+
 
 
 #Pause input
