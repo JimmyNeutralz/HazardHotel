030131e4ea9deb65b6e702836c617150f11c7ee8
SamDeShuckle <schreitersam@gmail.com>
2025-12-05 11:05:24 -0600

    Idk why the files arent importing lets try this

--- Sam the Cool One/curtain.gd
+++ Sam the Cool One/curtain.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
 var state
+@export var Player:Node
 @onready var Up = $Up
 @onready var Down = $Down
+signal gotocurtain
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	state = "up"
@@ -20,7 +22,17 @@ func _process(delta: float) -> void:
 	toggle_curtain()
 
 func toggle_curtain():
-	if Input.is_action_pressed("toggle_curtain"):
+	if Input.is_action_just_pressed("toggle_curtain"):
+		Player.move_to_object($Area3D)
+		if(!$Area3D.overlaps_body(Player)):
+			await(gotocurtain)
 		state = "down"
+	elif(Input.is_action_pressed("toggle_curtain")):
+		return
 	else:
 		state = "up"
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if(body == Player):
+		emit_signal("gotocurtain")
