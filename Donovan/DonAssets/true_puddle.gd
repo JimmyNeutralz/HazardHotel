e474c433dcccd33eb29ea648bd756bdf4f2d23b1
Komasangamer-coding <stolzd2061@my.uwstout.edu>
2025-12-08 22:00:56 -0600

    Updating textures as well as lamp functionality in the tutorial

--- Donovan/DonAssets/true_puddle.gd
+++ Donovan/DonAssets/true_puddle.gd
@@ -1,10 +1,11 @@
 extends Node3D
 
 @onready var mesh_instance = $Puddle/pasted__puddle.get_active_material(0)
-
+@onready var puddle_safe = load("res://ThomasFolder/Objects and Textures/HH_Art_PuddleNormTexture_V1.png")
+@onready var puddle_electrified = load("res://ThomasFolder/Objects and Textures/HH_Art_PuddleElecTexture_V1.png")
 
 func safe_puddle():
-	mesh_instance.albedo_color = Color(1.0, 1.0, 1.0, 1.0)
+	mesh_instance.set_surface_override_material(0, puddle_safe)
 
 func yellow_puddle():
-	mesh_instance.albedo_color = Color(0.75, 0.603, 0.141, 1.0)
+	mesh_instance.set_surface_override_material(0, puddle_electrified)
--- ThomasFolder/Scripts/lamp.gd
+++ ThomasFolder/Scripts/lamp.gd
@@ -21,10 +21,26 @@ func _ready() -> void:
 	directional_light.light_energy = 0.15
 	
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("turn_on_lamp") and !lamp_on:
-		activate_lights()
-	if Input.is_action_just_released("turn_on_lamp") and lamp_on:
-		deactivate_lights()
+	if !dialogue_started:
+		if Input.is_action_just_pressed("turn_on_lamp") and !lamp_on:
+			activate_lights()
+		if Input.is_action_just_released("turn_on_lamp") and lamp_on:
+			deactivate_lights()
+		
+		if (((player.global_position.x < (building_owner_marker.global_position.x + 0.5)) and (player.global_position.x >= (building_owner_marker.global_position.x - 0.5))) and lamp_on):
+			dialogue_started = true
+			player.move_to_specific_location(building_owner_marker.global_position.x)
+			text_popup.change_text_image(3)
+			text_popup.set_text("I was wondering who was ominously standing in the dark. I suppose you’re here to fix the electrical issue, right?", 6)
+			await get_tree().create_timer(5.85, false).timeout
+			if !generator_on:
+				text_popup.change_text_image(1)
+				text_popup.set_text("Yep, along with the electrician watching through the cameras.", 5)
+				await get_tree().create_timer(4.85, false).timeout
+				if !generator_on:
+					dialogue_finished = true
+					text_popup.change_text_image(3)
+					text_popup.set_text("Well, they’ll want to use that S.P.A.R.K board to activate that generator next to me the same way they activated the light.", 7)
 
 signal resume
 func activate_lights():
@@ -34,19 +50,7 @@ func activate_lights():
 	middle_light.light_energy = 1.
 	directional_light.light_energy = 1
 	if !dialogue_started:
-		dialogue_started = true
 		player.move_to_specific_location(building_owner_marker.global_position.x)
-		text_popup.change_text_image(3)
-		text_popup.set_text("I was wondering who was ominously standing in the dark. I suppose you’re here to fix the electrical issue, right?", 6)
-		await get_tree().create_timer(6.0, false).timeout
-		if !generator_on:
-			text_popup.change_text_image(1)
-			text_popup.set_text("Yep, along with the electrician watching through the cameras.", 5)
-			await get_tree().create_timer(5.0, false).timeout
-			if !generator_on:
-				dialogue_finished = true
-				text_popup.change_text_image(3)
-				text_popup.set_text("Well, they’ll want to use that S.P.A.R.K board to activate that generator next to me the same way they activated the light.", 7)
 
 func deactivate_lights():
 	lamp_on = false
