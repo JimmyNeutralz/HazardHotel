51a39c18948f2d1e33baeb6f8fcc5c3476a8cb25
Spencer <spencerbetchey@gmail.com>
2025-12-10 16:47:08 -0600

    Update from main, getting ready for V10

--- Donovan/Puzzle Concepts In Engine/Scenes/dumbwaiter.gd
+++ Donovan/Puzzle Concepts In Engine/Scenes/dumbwaiter.gd
@@ -18,7 +18,8 @@ func _process(delta):
 	#Handle safe deactivation input
 	#Global.check_array(5, 3) or 
 	
-	if (Global.check_array(5, 3) or Input.is_action_pressed("fix_dumbwaiter")) and isActivated == false:
+#	if (Global.check_array(5, 3) or Input.is_action_pressed("fix_dumbwaiter")) and isActivated == false:
+	if Input.is_action_pressed("fix_dumbwaiter") and isActivated == false:	
 		isActivated = true
 		player.move_to_object(standLoc)
 		uiNode.visible = false
