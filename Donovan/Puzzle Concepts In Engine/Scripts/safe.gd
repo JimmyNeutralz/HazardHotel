a596843e10bab4056bfff4477d81fb6e6664cc9a
JimmyNeutralz <hecimovichd9339@my.uwstout.edu>
2025-12-09 21:50:46 -0600

    Safe Functionality Update

--- Donovan/Puzzle Concepts In Engine/Scripts/safe.gd
+++ Donovan/Puzzle Concepts In Engine/Scripts/safe.gd
@@ -42,14 +42,13 @@ func _process(delta):
 	#Handle safe deactivation input
 	# if is_powered and and fuseBox.get_fuse_amount() >= 1
 	
-	if Input.is_action_pressed("lower_safe") and fuseBox.get_fuse_amount() >= 1:
+	if Input.is_action_pressed("left") and fuseBox.get_fuse_amount() >= 1:
 		if (first_pass):
 			first_pass = false
 			lower_safe()
-	elif Input.is_action_pressed("raise_safe") and fuseBox.get_fuse_amount() >= 1:
-		if (first_pass):
-			first_pass = false
-			raise_safe()
+	elif Input.is_action_just_released("left") and fuseBox.get_fuse_amount() >= 1:
+		raise_safe()
+		first_pass = true
 		
 	## Test for resource budgeting
 	#if (Global.check_array(1, 0)):
@@ -107,6 +106,7 @@ func lower_safe():
 	first_fuse_collected = true
 	player.move_to_object(fuseStandSpot)
 
+
 func raise_safe():
 	process_started = true
 	var tween = create_tween()
@@ -117,6 +117,8 @@ func raise_safe():
 	safe_raised = true
 	print("Safe Raised!")
 	process_started = false
+	
+	
 	
 
 ##Indicator Helpers
@@ -178,14 +180,14 @@ func open_safe():
 		
 		fuse2.visible = false
 		fuseBox.uiNode.visible = true;
-		first_pass = true
+	
 		
 func close_safe():
 	if anim_player and anim_player.has_animation("Take 001"):
 		anim_player.play_backwards("Take 001")
 		print("Safe closed!")
 		is_safe_open = false
-		first_pass = true
+
 		
 #Recursive search for AnimationPlayer
 func find_animation_player(node: Node) -> AnimationPlayer:
