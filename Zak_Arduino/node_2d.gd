b29683ee570b8dcd96194de98c761b14662fe9f5
ZacharySuhajda <suhajdaz3219@my.uwstout.edu>
2025-12-10 16:28:12 -0600

    Revert "Merge branch 'NewWorkingMain' into DonovanBranch"
    This reverts commit a819ce3d1b11a47ff02c25196b97f5304ad8b1d4, reversing
    changes made to bfd149f9b2984c3208dcd379b7e0399171225ad1.

--- Zak_Arduino/node_2d.gd
+++ Zak_Arduino/node_2d.gd
@@ -5,32 +5,14 @@ extends Node2D
 @onready var text1 = $RichTextLabel
 @onready var text2 = $RichTextLabel2
 
-var temp = []
-var str
-
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	#temp = string_to_bool_grid(MyCSharpScript.Powered_String)
-	
-	text1.text = str(MyCSharpScript.getPower(0,0))
+	text1.text = MyCSharpScript.text1;
 	text2.text = MyCSharpScript.text2;
 	
-func string_to_bool_grid(data: String) -> Array:
-	if data.length() != 48:
-		push_error("Input must be exactly 48 characters.")
-		return []
-
-	var result := []
-	var index := 0
-
-	for row in range(8):
-		var row_array := []
-		for col in range(6):
-			var ch := data[index]
-			row_array.append(ch == "T")
-			index += 1
-			result.append(row_array)
-	return result
+func _physics_process(_delta: float) -> void:
+	print("|" + MyCSharpScript.serialMessage + "|")
+	pass
