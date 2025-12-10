06064cf891ef0a631f807a90d849a270dee65b97
Komasangamer-coding <stolzd2061@my.uwstout.edu>
2025-12-09 23:55:48 -0600

    Working away on getting the new generator code set up

--- SpencerStuff/Scripts/generator.gd
+++ SpencerStuff/Scripts/generator.gd
@@ -7,12 +7,14 @@ extends Node3D
 @onready var elevator_lock = $"../ElevatorDoor/ElevatorLock"       
 @onready var uiNode = $"../Gate/GateUI"     
 @onready var uiNode2 = $GeneratorUI
-
+@onready var player = $"../Player"
 
 @onready var text = $"../Overlay/TextPopup"                    
 
 var gate : Node3D = null
 var activated = false
+
+var can_interact = false
 
 func _ready():
 	if gate_node_path != null:
@@ -21,16 +23,32 @@ func _ready():
 		push_error("Gate node path not set for Generator!")
 
 func _process(delta):
-	if Input.is_action_just_pressed("activate_generator") and not activated:
+	if Input.is_action_just_pressed("activate_generator") and not can_interact:
 		if can_activate():
+			
 			activate_generator()
-			uiNode.visible = false
-			uiNode2.visible = false
-			text.change_text_image(1)
-			text.set_text("Generator up and running for this floor. Better head back to the elevator.", 6)
+			#uiNode.visible = false
+			#uiNode2.visible = false
+			#text.change_text_image(1)
+			#text.set_text("Generator up and running for this floor. Better head back to the elevator.", 6)
 		else:
 			print("Cannot activate generator yet!")
-			
+	if Input.is_action_just_released("activate_generator") and can_interact:
+		deactivate_generator()
+		
+	if !activated:
+		if (((player.global_position.x >= (global_position.x + 0.25)) and (player.global_position.x >= (global_position.x - 0.25))) and can_interact):
+			print("Generator activated!")
+			$GeneratorAudio.play()
+			activated = true
+		
+			uiNode.visible = false
+			text.change_text_image(3)
+			complete_generator_text()
+		#elif (((player.global_position.x < (global_position.x + 0.25)) and (player.global_position.x >= (global_position.x - 0.25))) and !can_interact):
+			#lamp.generator_on = true
+			#text.change_text_image(3)
+			#incomplete_tutorial_generator_text()
 	## Test for resource budgeting
 	#if (Global.check_array(1, 0)):
 		#uiNode.visible = true
@@ -49,9 +67,12 @@ func can_activate() -> bool:
 	return gate_raised
 
 func activate_generator():
-	activated = true
-	$GeneratorAudio.play()
-	print("Generator activated!")
+	can_interact = true
+	
+	player.move_to_specific_location(self.global_position.x)
+
+func deactivate_generator():
+	can_interact = false
 
 	##change color
 	#if indicator:
